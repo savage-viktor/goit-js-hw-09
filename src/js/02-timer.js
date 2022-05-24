ca9e8130b29b112933e17a4a65c18350c528bb25
savage-viktor <viktor.dkn@gmail.com>
2022-05-24 07:50:47 +0300

    Task 2, 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,85 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css";
 
+import Notiflix from 'notiflix';
+
+
+Notiflix.Notify.init({
+    width: '280px',
+    position: 'center-center', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
+});
+
+
+const daysField = document.querySelector(".value[data-days]")
+const hoursField = document.querySelector(".value[data-hours]")
+const minutesField = document.querySelector(".value[data-minutes]")
+const secondsField = document.querySelector(".value[data-seconds]")
+const startButton = document.querySelector("button[data-start]")
+
+
+let targetDate;
+let startDate;
+let oddsDate;
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
+        targetDate = new Date(selectedDates[0]).getTime()
     },
 };
 
+function startFn() {
+    startDate = new Date().getTime()
+
+    if (targetDate - startDate <= 0) {
+        Notiflix.Notify.failure('Please choose a date in the future');
+        return
+    }
+
+
+    const tamerId = setInterval(() => {
+
+        startDate = new Date().getTime()
+        oddsDate = targetDate - startDate
+
+        if (oddsDate < 0) {
+            Notiflix.Notify.success('Time expired');
+            clearInterval(tamerId)
+            return
+        }
+
+        const { days, hours, minutes, seconds } = convertMs(oddsDate)
+
+        daysField.textContent = days;
+        hoursField.textContent = hours;
+        minutesField.textContent = minutes;
+        secondsField.textContent = seconds;
+    }, 1000)
+}
+
+startButton.addEventListener("click", startFn)
 
 flatpickr("#datetime-picker", options)
 
 options.onClose
 
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector(`form`)
+
+formRef.addEventListener("submit", onSubmit)
+
+function onSubmit(event) {
+  event.preventDefault()
+
+  const firstDelay = Number(event.currentTarget.elements.delay.value);
+  const delayStep = Number(event.currentTarget.elements.step.value);
+  const amount = Number(event.currentTarget.elements.amount.value);
+
+  const startDelay = firstDelay - delayStep;
+  const delaysArray = createDelaysArray(amount, startDelay, delayStep)
+
+  runAllPromises(delaysArray)
+}
+
+function createDelaysArray(amount, startDelay, delayStep) {
+  const delaysArray = [];
+  for (let i = 1; i <= amount; i += 1) {
+    startDelay += delayStep
+    delaysArray.push(startDelay)
+  }
+  return delaysArray
+}
+
+function runAllPromises(delaysArray) {
+  delaysArray.map((delay, index) => {
+    createPromise(index + 1, delay)
+      .then(res => { Notiflix.Notify.success(res) })
+      .catch(err => { Notiflix.Notify.failure(err) })
+  })
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      }
+      else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`)
+      }
+    }, delay)
+  })
 }
